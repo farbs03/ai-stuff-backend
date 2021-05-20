a78fd21ba5f1a446ecbaeffdb8ba09a0590e36f3
farbs03 <cgeraldfarber@icloud.com>
2021-05-20 18:25:44 -0400

    for the love of god please work

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
 from werkzeug.utils import cached_property
 from flask import Flask, request, jsonify, make_response
+from flask_restful import Api, Resource
+import sys
 import summarizer
-from flask_cors import CORS, cross_origin
-import os
+
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-port = int(os.environ.get('PORT', 5000))
+api = Api(app)
 
-@app.route('/summarize', methods=['POST'])
-@cross_origin()
-def summarize():
-	response = request.json
-	if(request.method == "POST"):
-		return jsonify({
-			"statusCode": 200,
-			"status": "Success",
-			"result": summarizer.getSummary(response)
-		})
-	else:
-		return jsonify({
-			"ERROR": "No data found"
-		})
+class GenerateSummary(Resource):
+
+	def options(self):
+		response = make_response()
+		response.headers.add("Access-Control-Allow-Origin", "*")
+		response.headers.add('Access-Control-Allow-Headers', "*")
+		response.headers.add('Access-Control-Allow-Methods', "*")
+		return response
+	
+	def post(self):
+		try: 
+			formData = request.json
+			response = jsonify({
+				"statusCode": 200,
+				"status": "Prediction made",
+				"result": summarizer.getSummary(formData)
+			})
+			response.headers.add('Access-Control-Allow-Origin', '*')
+			return response
+		except Exception as error:
+			return jsonify({
+				"statusCode": 500,
+				"status": "Could not make prediction",
+				"error": str(error)
+			})
+
+api.add_resource(GenerateSummary, '/summarize')
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port = 5000, debug=True)
+	app.run(debug=True)
